9c39b2ba6a706fd238e84d7779cd67e5be5a5102
fprtmjinho <fjrtmjinho@gmail.com>
2024-01-05 14:29:37 +0900

    [FEAT] 바텀 네비게이션 화면 이동 구현

--- app/src/main/java/com/example/daon/MainActivity.kt
+++ app/src/main/java/com/example/daon/MainActivity.kt
@@ -10,7 +10,47 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         binding = ActivityMainBinding.inflate(layoutInflater)
         setContentView(binding.root)
-//        initBottomNavigation()
+        initBottomNavigation()
     }
+    private fun initBottomNavigation(){
+        supportFragmentManager.beginTransaction()
+            .replace(R.id.main_frm, CalendarFragment())
+            .commit()
+        binding.mainBnv.setOnItemSelectedListener{ item ->
+            when (item.itemId) {
+                R.id.calendarFragment -> {
+                    val homeFragment = CalendarFragment()
+                    supportFragmentManager
+                        .beginTransaction()
+                        .replace(R.id.main_frm, homeFragment)
+                        .commit()
+                    return@setOnItemSelectedListener true
+                }
 
+                R.id.diaryFragment -> {
+                    val lookFragment = DiaryFragment()
+                    supportFragmentManager
+                        .beginTransaction()
+                        .replace(R.id.main_frm, lookFragment)
+                        .commit()
+                    return@setOnItemSelectedListener true
+                }
+                R.id.communityFragment -> {
+                    val searchFragment = CommunityFragment()
+                    supportFragmentManager.beginTransaction()
+                        .replace(R.id.main_frm, searchFragment)
+                        .commitAllowingStateLoss()
+                    return@setOnItemSelectedListener true
+                }
+                R.id.mypageFragment -> {
+                    val lockerFragment = MypageFragment()
+                    supportFragmentManager.beginTransaction()
+                        .replace(R.id.main_frm, lockerFragment)
+                        .commitAllowingStateLoss()
+                    return@setOnItemSelectedListener true
+                }
+            }
+            false
+        }
+    }
 }
