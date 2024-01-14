9d8ac8a0236ef08bca0d81918b7d3bf6d954fd5b
fprtmjinho <fjrtmjinho@gmail.com>
2024-01-14 18:41:00 +0900

    [FEAT] fragment_body.xml 제작, editText에 사용할 디자인 제작, fragment_dose.xml에 사용할 버튼 디자인 제작

--- app/src/main/java/com/example/daon/fab/BodyFragment.kt
+++ app/src/main/java/com/example/daon/fab/BodyFragment.kt
@@ -5,9 +5,7 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
-import com.example.daon.R
 import com.example.daon.databinding.FragmentBodyBinding
-
 
 class BodyFragment : Fragment() {
     private var _binding: FragmentBodyBinding? = null
