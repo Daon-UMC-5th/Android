381282391b31d3497e640c3208dabaa36d78a03b
fprtmjinho <fjrtmjinho@gmail.com>
2024-01-28 16:41:31 +0900

    [FEAT] 일기 UI 제작

--- app/src/main/java/com/example/daon/main/DiaryFragment.kt
+++ app/src/main/java/com/example/daon/main/DiaryFragment.kt
@@ -1,19 +1,57 @@
 package com.example.daon.main
 
+import android.graphics.Color
 import android.os.Bundle
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import com.example.daon.MyApplication
 import com.example.daon.R
+import com.example.daon.databinding.FragmentDiaryBinding
+import com.prolificinteractive.materialcalendarview.CalendarDay
+import kotlin.properties.Delegates
 
 class DiaryFragment : Fragment() {
+    private lateinit var _binding: FragmentDiaryBinding
+    private val binding get() = _binding!!
+    private var noticeCnt: Int = 0
+    private var year: Int = 0
+    private var month: Int = 0
+    private var day: Int = 0
 
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_diary, container, false)
+        _binding = FragmentDiaryBinding.inflate(inflater,container,false)
+        initNotice()
+        initStatement()
+        initCalendar()
+        return binding.root
     }
+    private fun initNotice(){
+        if (noticeCnt==0) binding.notice.setImageResource(R.drawable.notice)
+        else binding.notice.setImageResource(R.drawable.notice_true)
+    }
+    private fun initStatement(){
+        initDiaryStatement()
+        initCalendar()
+    }
+    private fun initDiaryStatement(){
+        when(MyApplication.preferences.getStatement("diaryStatement","개인")){
+            "개인" ->{
+
+            }
+            "공유" ->{
+
+            }
+        }
+    }
+    private fun initCalendar(){
+//        year = MyApplication.preferences.getDate("diaryPrivateYear",CalendarDay.today().year)
+//        month = MyApplication.preferences.getDate("diaryPrivateMonth",CalendarDay.today().month)
+//        day = MyApplication.preferences.getDate("diaryPrivateDay",CalendarDay.today().day)
+    }
+
 }
