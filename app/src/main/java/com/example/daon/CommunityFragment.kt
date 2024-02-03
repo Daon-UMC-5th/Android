166d2a314376db1b670fabe3610612e1bf975b77
kwon-h-chan <a71179447@gmail.com>
2024-02-04 02:30:14 +0900

    test

--- app/src/main/java/com/example/daon/CommunityFragment.kt
+++ app/src/main/java/com/example/daon/CommunityFragment.kt
@@ -8,6 +8,7 @@ import android.view.LayoutInflater
 import android.view.MenuItem
 import android.view.View
 import android.view.ViewGroup
+import android.widget.ImageView
 import androidx.core.content.ContextCompat
 import androidx.core.view.GravityCompat
 import androidx.drawerlayout.widget.DrawerLayout
