945bb001faa4dae4128caa22f98d4e63378eba3c
kwon-h-chan <a71179447@gmail.com>
2024-02-07 18:56:00 +0900

    마이페이지 프로필 설정

--- app/src/main/java/com/example/mypage2/PagealarmActivity.kt
+++ app/src/main/java/com/example/mypage2/PagealarmActivity.kt
@@ -17,5 +17,22 @@ class PagealarmActivity : AppCompatActivity() {
         binding.alarmBack.setOnClickListener {
           onBackPressedDispatcher.onBackPressed()
         }
+        binding.swtAll.setOnCheckedChangeListener { _, isChecked ->
+            if (isChecked) {
+                // switch1이 체크되면 나머지 스위치들도 체크
+                binding.swt01.isChecked = true
+                binding.swt02.isChecked = true
+                binding.swt03.isChecked = true
+                binding.swt04.isChecked = true
+                binding.swt05.isChecked = true
+            }
+            else{
+                binding.swt01.isChecked = false
+                binding.swt02.isChecked = false
+                binding.swt03.isChecked = false
+                binding.swt04.isChecked = false
+                binding.swt05.isChecked = false
+            }
+        }
     }
 }
