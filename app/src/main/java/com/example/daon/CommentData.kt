8eb30b33408b4fa55651e5afe8d3387fe588edde
kwon-h-chan <a71179447@gmail.com>
2024-02-08 04:53:09 +0900

    댓글 액티비티

---
+++ app/src/main/java/com/example/daon/CommentData.kt
@@ -0,0 +1,13 @@
+package com.example.daon
+
+data class CommentData(
+    val nickname: String,
+    val intro: String,
+    val icon: Int,
+    val timeAgo: String,
+    val profileImage: Int,
+    val more: Int,
+    val detail: String,
+    val favorIcon: Int,
+    val favorCount: String,
+)
--- app/src/main/java/com/example/daon/ReadwriteActivity.kt
+++ app/src/main/java/com/example/daon/ReadwriteActivity.kt
@@ -1,11 +1,25 @@
 package com.example.daon
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.example.daon.databinding.ActivityReadwriteBinding
+import com.example.daon.databinding.ActivityWriteBinding
 
 class ReadwriteActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityReadwriteBinding
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_readwrite)
+        binding = ActivityReadwriteBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        binding.writeBack.setOnClickListener {
+            onBackPressedDispatcher.onBackPressed()
+        }
+
+        binding.commentBackground.setOnClickListener {
+            val intent = Intent(this,CommentActivity::class.java)
+            startActivity(intent)
+        }
     }
 }
