d58cc0b934b7a8fa843dd86f544d379fb3342727
fprtmjinho <fjrtmjinho@gmail.com>
2024-02-09 23:16:05 +0900

    [feat] 캘린더 통신 복용 기본 세팅

--- app/src/main/java/com/example/daon/conect/calendar/CalendarService.kt
+++ app/src/main/java/com/example/daon/conect/calendar/CalendarService.kt
@@ -7,32 +7,45 @@ import retrofit2.http.GET
 import retrofit2.http.POST
 import retrofit2.http.PUT
 import retrofit2.http.Path
-import java.util.Date
 
 interface CalendarService {
     //캘린더
     //신체 조회
     @GET("calendar/physical-record/{date}")
-    fun bodyListCall(@Path("date") date: Date): Call<BodyListCallResponseDto>
+    fun bodyListCall(@Path("date") date: String): Call<BodyListCallResponseDto>
     //신체 삽입
     @POST("calendar/physical-record/{date}")
-    fun bodyListInsert(@Path("date") date: Date, @Body request: BodyListInsertRequestDto): Call<BodyListInsertResponseDto>
+    fun bodyListInsert(@Path("date") date: String, @Body request: BodyListInsertRequestDto): Call<BodyListInsertResponseDto>
     //신체 삭제
     @DELETE("calendar/physical-record/{date}")
-    fun bodyListDelete(@Path("date") date: Date) : Call<BodyListInsertResponseDto>
+    fun bodyListDelete(@Path("date") date: String) : Call<BodyListInsertResponseDto>
     //신체 수정
     @PUT("calendar/physical-record/{date}")
-    fun bodyListEdit(@Path("date") date: Date, @Body request: BodyListInsertRequestDto) : Call<BodyListInsertResponseDto>
+    fun bodyListEdit(@Path("date") date: String, @Body request: BodyListInsertRequestDto) : Call<BodyListInsertResponseDto>
     //진료 조회
     @GET("calendar/consultation/{date}")
-    fun clinicListCall(@Path("date") date: Date) : Call<ClinicListCallResponseDto>
+    fun clinicListCall(@Path("date") date: String) : Call<ClinicListCallResponseDto>
     //진료 삽입
     @POST("calendar/consultation/{date}")
-    fun clinicListInsert(@Path("date") date: Date, @Body request: ClinicListInsertRequestDto) : Call<ClinicListInsertResponseDto>
+    fun clinicListInsert(@Path("date") date: String, @Body request: ClinicListInsertRequestDto) : Call<ClinicListInsertResponseDto>
     //진료 삭제
     @DELETE("calendar/consultation/{date}")
-    fun clinicListDelete(@Path("date") date: Date) : Call<ClinicListInsertResponseDto>
+    fun clinicListDelete(@Path("date") date: String) : Call<ClinicListInsertResponseDto>
     //진료 수정
     @PUT("calendar/consultation/{date}")
-    fun clinicListEdit(@Path("date") date: Date, @Body request: ClinicListInsertRequestDto) : Call<ClinicListInsertResponseDto>
+    fun clinicListEdit(@Path("date") date: String, @Body request: ClinicListInsertRequestDto) : Call<ClinicListInsertResponseDto>
+    //복용 조회
+    @GET("calendar/madication/{date}")
+    fun doseAllListCall(@Path("date") date: String) : Call<DoseAllListCallResponseDto>
+    @GET("calendar/madication/{when}/{date}")
+    fun doseListCall(@Path("when") time: String, @Path("date") date: String) : Call<DoseAllListCallResponseDto>
+    //복용 삽입
+    @POST("calendar/madication/{when}/{date}")
+    fun doseListCall(@Path("when") time: String,@Path("date") date: String, @Body request: DoseListInsertRequestDto) : Call<DoseListInsertResponseDto>
+    //복용 삭제
+    @DELETE("calendar/madication/{when}/{date}")
+    fun doseListDelete(@Path("when") time: String,@Path("date") date: String) : Call<DoseListInsertResponseDto>
+    //복용 수정
+    @PUT("calendar/madication/{when}/{date}")
+    fun doseListEdit(@Path("when") time: String,@Path("date") date: String, @Body request: DoseListInsertRequestDto) : Call<DoseListInsertResponseDto>
 }
